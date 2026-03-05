05518b74b28c72167d6fb4a121a7adce5e7d3e05
cobumusic <jacobmugalde@gmail.com>
2026-03-05 12:26:00 -0800

    test commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Nav from '../src/components/Nav/Nav'
-import Hero from '../src/components/Hero/Hero'
-import About from '../src/components/About/About'
-import Experience from '../src/components/Experience/Experience'
-import Skills from '../src/components/Skills/Skills'
-import Work from '../src/components/Work/Work'
-import Contact from '../src/components/Contact/Contact'
-import Footer from '../src/components/Footer/Footer'
+import Nav from "../src/components/Nav/Nav";
+import Hero from "../src/components/Hero/Hero";
+import About from "../src/components/About/About";
+import Experience from "../src/components/Experience/Experience";
+import Skills from "../src/components/Skills/Skills";
+import Work from "../src/components/Work/Work";
+import Contact from "../src/components/Contact/Contact";
+import Footer from "../src/components/Footer/Footer";
 
 export default function Home() {
   return (
@@ -13,7 +13,7 @@ export default function Home() {
       <Nav />
       <main>
         <Hero />
-        <About />
+        {/* <About /> */}
         <Experience />
         <Skills />
         <Work />
@@ -21,5 +21,5 @@ export default function Home() {
       </main>
       <Footer />
     </>
-  )
+  );
 }
